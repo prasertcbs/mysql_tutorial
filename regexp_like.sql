7bf626048abb337ab54b1a28faf2fcc22247a9fc
prasertcbs <bluecotton.k@gmail.com>
2020-11-23 21:07:52 +0700

    Add files via upload

--- regexp_like.sql
+++ regexp_like.sql
@@ -1,16 +1,18 @@
 -- สอน MySQL 8: basic regular expression
 -- doc: https://dev.mysql.com/doc/refman/8.0/en/regexp.html
--- sql script: 
+-- sql script: https://github.com/prasertcbs/mysql_tutorial/blob/main/regexp_like.sql
+-- create 'movies' table script: https://github.com/prasertcbs/mysql_tutorial/blob/main/data/movies.sql
 -- YouTube: 
 -- Author: Prasert Kanawattanachai
 -- email: prasert.k@chula.ac.th
 
--- create 'movies' table script: https://github.com/prasertcbs/mysql_tutorial/blob/main/data/movies.sql
-select title from movies limit 50; 
+select * from movies limit 50; 
+
+-- like
+select title from movies where title like '%iron%'; 
 
 -- contains
 -- rlike is case insensitive
-select title from movies where title like '%iron%'; 
 
 -- three different ways
 select title from movies where title rlike 'iron';
@@ -31,25 +33,53 @@ select title from movies where title rlike 'iron|spider|bat';
 select title from movies 
     where (title like '%iron%') or (title like '%spider%') or (title like '%bat%');
 
-select title from movies 
+select title, description from movies 
     where (description rlike 'love') or (title rlike 'love');
 
--- starts with
+-- starts with ^
 select title from movies where title rlike '^man';
+select title from movies where title rlike 'man';
 
--- word boundary
+-- word boundary with \b
+
+-- word starts with man
+
+select title from movies where title rlike '\\bman';
+
+-- word ends with man (batman, superman)
+select title from movies where title rlike 'man\\b';
+
+-- word 'man'
+select title from movies where title rlike '\\bman\\b';
+
+-- starts with word 'man'
 select title from movies where title rlike '^\\bman\\b';
 
--- ends with
+select title from movies where title rlike '^\\bman';
+select title from movies where title rlike '^\\bman\\b';
+
+-- ends with $
 select title from movies where title rlike 'man$';
 select title from movies where title rlike '\\d$';
 select title from movies where title rlike '3$';
+select title from movies where title rlike '(3|iii)$';
 select title from movies where title rlike '\\b3\\b$';
 select title from movies where title rlike '^t.*\\d$';
 
+-- escape metacharacter
+select title from movies where title rlike '.';
+select title from movies where title rlike '\\.';
+select title from movies where title rlike 'g.i.';
+select title from movies where title rlike 'g\\.i\\.';
+-- select title from movies where title rlike '(';
+select title from movies where title rlike '\\(';
+
 -- () -> group
+select title from movies where title rlike 'spider|bat|iron';
 select title from movies where title rlike '(spider|bat|iron)';
 select title from movies where title rlike '^(spider|bat|iron)';
+select title from movies where title rlike '(2|3)$';
+select title from movies where title rlike '(\\b2|\\b3)$';
 
 -- contains 'three' or ends with 3
 select title from movies where title rlike 'three|3$';
@@ -64,11 +94,16 @@ select title from movies where title rlike '^i.*(three|3|iii)$';
 select title from movies where title rlike '^s.*(three|3|iii)$';
 
 -- match one of a set of characters []
+-- man men
+select title from movies where title rlike 'man|men';
 select title from movies where title rlike 'm[a|e]n';
+select title from movies where title rlike 'm[ae]n';
 select title from movies where title rlike '^\\bm[a|e]n\\b';
 
 -- downey AND evans
+select title, actors from movies limit 10;
 select title, actors from movies where actors rlike 'downey.*evans';
+select title, actors from movies where actors rlike 'evans.*downey';
 
 select title, actors from movies where actors rlike 'downey.*evans|evans.*downey';
 
@@ -87,12 +122,12 @@ select title from movies where title rlike 'i{2}';
 select title from movies where title rlike 'i{1,2}';
 select title from movies where title rlike 'i{2,}';
 
--- title, ?, +
--- atitle -> a{0,}
+-- *, ?, +
+-- a* -> a{0,}
 -- a+ -> a{1,} (at least one)
 -- a? -> a{0,1}
 
-select title from movies where title rlike '0title';
+select title from movies where title rlike '0*';
 select title from movies where title rlike '0+';
 select title from movies where title rlike '0?';
 
@@ -123,3 +158,8 @@ update m
 select title from m where title rlike 'spider';
 select title from m where title rlike 'spider-?man';
 
+-- white space \s
+-- find extra space
+select regexp_like('lily  carnation\t\trose', '\\s{2,}');
+select regexp_like('lily carnation\trose', '\\s{2,}');
+select 'lily carnation rose';
